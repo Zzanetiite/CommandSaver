fca706e8c0983d3ec3decc3f07788c56f9bb1dbf
Zanete Dijeva <zanete.dijeva@gmail.com>
2023-10-15 23:05:31 +0100

    Description added in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="command-saver-terminal-program",
-    version="2.0.2",
+    version="2.0.3",
+    description="Simple terminal helper program to save long terminal commands. See git: https://github.com/Zzanetiite/CommandSaver",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
